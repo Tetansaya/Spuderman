16957958c2373f755cd6ad420f66efb4db882621
Tetansaya <a1120120068@utch.edu.mx>
2024-01-30 17:47:33 -0600

    final

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 // Middleware para manejar datos JSON en las solicitudes
 app.use(bodyParser.json());
 
-// Datos de ejemplo
+// Datos 
 const animales = {
   lunes: ['León', 'Elefante', 'Jirafa'],
   martes: ['Delfín', 'Ballena', 'Tiburón'],
@@ -14,7 +14,7 @@ const animales = {
   jueves: ['Colibrí', 'Águila Real', 'Gaviota'],
   viernes: ['Iguana', 'Cobra', 'Tortuga'],
   sabado: ['Cocodrilo', 'Tortuga Marina', 'Anaconda'],
-  domingo: ['Salamandra', 'Rana Flecha Azul', 'Axolotl'],
+  domingo: ['Salamandra', 'Rana Flecha Azul', 'Ajolote'],
 };
 
 // Obtener todos los animales de un día
@@ -66,7 +66,50 @@ app.delete('/animal/:dia/:indice', (req, res) => {
   }
 });
 
+// función para obtener todos los días disponibles
+app.get('/dias', (req, res) => {
+  const dias = Object.keys(animales);
+  res.json({ diasDisponibles: dias });
+});
+
+// función para listar todos los animales aéreos
+app.get('/animales-aereos', (req, res) => {
+  const animalesAereos = [].concat(animales.miercoles, animales.jueves);
+  res.json({ animalesAereos });
+});
+
+// función para listar todos los animales acuáticos
+app.get('/animales-acuaticos', (req, res) => {
+  const animalesAcuaticos = [].concat(animales.martes, animales.sabado);
+  res.json({ animalesAcuaticos });
+});
+
+// función para listar todos los animales terrestres
+app.get('/animales-terrestres', (req, res) => {
+  const animalesTerrestres = [].concat(animales.lunes, animales.viernes, animales.domingo);
+  res.json({ animalesTerrestres });
+});
+
 // Inicia el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
+/*
+animales del dia (Lunes): GET http://localhost:3000/animal/lunes
+
+añadir un nuevo animal al dia (Lunes): POST http://localhost:3000/animal/lunes
+
+actualizar un animal de un dia (Lunes)(Animal en posicion 0): PUT http://localhost:3000/animal/lunes/0 
+
+eliminar un animal de un dia (Lunes)(Animal en la posicion 0): DELETE  http://localhost:3000/animal/lunes/0
+
+todos los días disponibles: GET http://localhost:3000/dias
+
+todos los animales aéreos: GET http://localhost:3000/animales-aereos
+
+todos los animales acuáticos: GET http://localhost:3000/animales-acuaticos
+
+todos los animales terrestres: GET http://localhost:3000/animales-terrestres
+*/
